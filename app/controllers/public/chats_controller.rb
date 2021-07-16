56efc733bab2b08e276200458d85149a81546fbd
momose1031 <macbookair2020momose@icloud.com>
2021-07-16 19:45:50 +0000

    continue DM

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -1,6 +1,6 @@
 class Public::ChatsController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def show
     @customer = Customer.find(params[:id])
     rooms = current_customer.customer_rooms.pluck(:room_id)
@@ -17,17 +17,21 @@ class Public::ChatsController < ApplicationController
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
-  
+
   def create
     @chat = current_customer.chats.new(chat_params)
     @chat.save
-    redirect_to request.referer
+    
+    @customer = Customer.find(params[:id])
+    @room = CustomerRoom.find_by(customer_id: @customer_id)
+    @chats = Chat.find_by(room_id: @room.id)
+    # redirect_to request.referer
   end
 
   private
-  
+
   def chat_params
     params.require(:chat).permit(:message, :room_id)
   end
-  
+
 end
