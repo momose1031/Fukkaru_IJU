4cd28f5e4d216c073463a742e8017f4fdcfd98f4
momose1031 <macbookair2020momose@icloud.com>
2021-07-16 17:39:49 +0000

    follow ajax

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -3,12 +3,14 @@ class Public::RelationshipsController < ApplicationController
   
   def create
     current_customer.follow(params[:customer_id])
-    redirect_to request.referer
+    @customer = Customer.find(params[:customer_id])
+    # redirect_to request.referer
   end
 
   def destroy
     current_customer.unfollow(params[:customer_id])
-    redirect_to request.referer
+    @customer = Customer.find(params[:customer_id])
+    # redirect_to request.referer
   end
   
   def followings
