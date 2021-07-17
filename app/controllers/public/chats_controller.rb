354da62504b58c27e8ed41f0f9c3ee9cd9fcec67
momose1031 <macbookair2020momose@icloud.com>
2021-07-17 07:12:55 +0000

    finish DM ajax

--- app/controllers/public/chats_controller.rb
+++ app/controllers/public/chats_controller.rb
@@ -2,30 +2,27 @@ class Public::ChatsController < ApplicationController
   before_action :authenticate_customer!
 
   def show
-    @customer = Customer.find(params[:id])
-    rooms = current_customer.customer_rooms.pluck(:room_id)
+    @customer = Customer.find(params[:id]) #相手の会員IDを取得
+    rooms = current_customer.customer_rooms.pluck(:room_id) #自分の会員ルームのルームIDを
     customer_rooms = CustomerRoom.find_by(customer_id: @customer.id, room_id: rooms)
 
-    unless customer_rooms.nil?
+    unless customer_rooms.nil? #ルームがあれば@roomに代入
       @room = customer_rooms.room
-    else
+    else #ルームがなければ新しく作る
       @room = Room.new
       @room.save
-      CustomerRoom.create(customer_id: current_customer.id, room_id: @room.id)
-      CustomerRoom.create(customer_id: @customer.id, room_id: @room.id)
+      CustomerRoom.create(customer_id: current_customer.id, room_id: @room.id) #相手の会員ルームのレコードを追加
+      CustomerRoom.create(customer_id: @customer.id, room_id: @room.id) #自分の会員ルームのレコードを追加
     end
     @chats = @room.chats
     @chat = Chat.new(room_id: @room.id)
   end
 
   def create
+    @room = Room.find(params[:chat][:room_id])
+    @chats = @room.chats
     @chat = current_customer.chats.new(chat_params)
     @chat.save
-    
-    @customer = Customer.find(params[:id])
-    @room = CustomerRoom.find_by(customer_id: @customer_id)
-    @chats = Chat.find_by(room_id: @room.id)
-    # redirect_to request.referer
   end
 
   private
