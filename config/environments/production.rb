853151a2a94eb9ba04d5dd3432b8c0c34971e467
momose1031 <macbookair2020momose@icloud.com>
2021-07-22 19:57:14 +0000

    fix due to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
@@ -93,3 +93,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.assets.precompile += ['homes_about.js']
 end
+
