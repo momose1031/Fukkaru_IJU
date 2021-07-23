c3d0c8b96b4e03efd9cfa23970bc49db71aeac84
momose1031 <macbookair2020momose@icloud.com>
2021-07-23 10:22:04 +0000

    fix test deploy

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.all.order(created_at: :desc).limit(4)
+    # @posts = Post.all.order(created_at: :desc).limit(4)
   end
 
   def about
