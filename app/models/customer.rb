9547ab6b51c8fac56b552001feea8c09c69e61f7
momose1031 <macbookair2020momose@icloud.com>
2021-07-28 15:38:13 +0000

    test deploy

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,31 +3,31 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :post
   has_many :comments, dependent: :destroy
   has_many :customer_rooms
   has_many :chats
- 
+
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy  #自分がフォローされる側
   has_many :followers, through: :reverse_of_relationships, source: :follower #自分をフォローしている人
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy #自分がフォローする側
   has_many :followings, through: :relationships, source: :followed #自分がフォローしている人
-  
+
   enum conutry: { 県民の方: 0, 移住に興味のある方: 1 }
-  
+
   attachment :image
-  
+
   def follow(customer_id) #フォローする
     relationships.create(followed_id: customer_id)
   end
-  
+
   def unfollow(customer_id) #フォロー解除
     relationships.find_by(followed_id: customer_id).destroy
   end
-  
+
   def following?(customer) #フォローしているか確認
     followings.include?(customer)
   end
@@ -35,7 +35,7 @@ class Customer < ApplicationRecord
   def active_for_authentication?
     super && (self.is_deleted == false)
   end
-  
+
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |customer|
     customer.password = SecureRandom.urlsafe_base64
@@ -44,12 +44,12 @@ class Customer < ApplicationRecord
     customer.is_deleted = false
     end
   end
-  
+
   validates :name, :presence => {:message => 'ニックネームを入力してください'}
   validates :email, :presence => {:message => 'メールアドレスを入力してください'}
   validates :password, :presence => {:message => 'パスワードを入力してください'}
-  
-  validates :name, length: { maximum: 2}
+
+  validates :name, length: { maximum: 20}
   validates :introduction, length: { maximum: 200}
-  
+
 end
